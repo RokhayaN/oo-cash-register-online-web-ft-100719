2358297dee7e6f0db26a9b53b07adc73ffcf445b
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 23:40:51 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,11 @@
+class CashRegister
+attr_accessor :discount,:total  
 
+def intialize (discount=nil)
+  @discount = discount
+  @total = 0
+end 
+def add_item(item, price, amount=1)
+    #binding.pry
+    self.total += price * amount
+  end
