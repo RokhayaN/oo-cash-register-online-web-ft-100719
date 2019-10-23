3af1a7ec58c13e4587a75bcb69a9730bc70636cd
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-23 23:48:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,3 +9,12 @@ def add_item(item, price, amount=1)
     # binding.pry
     self.total += price * amount
   end
+  def apply_discount
+    # binding.pry
+    if discount > 0 && total
+      self.total = (self.total * ((100.0 - discount.to_f)/100)).to_i
+    else
+      self.total
+    end
+  end
+end 
